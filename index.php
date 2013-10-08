cada7a87c58b7f0518bc08e364323a0810504ca3
Israel <isracortes62@gmail.com>
2013-10-08 13:37:20 -0500

    Subiendo index nuevo

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Club Valle Imperial</title>
+	<h1>Bienvenido</h1>
+	
+    <!--<link rel="stylesheet" href="app/css/bootstrap.css">
+    <link rel="stylesheet" href="app/css/main.css"> --!>
+  </head>
+  <body>
+    <div id="APPContainer">
+      <section>
+        <h3>Valle Imperial Testing Index</h3>      
+      </section>
+    </div>
+  </body>
+  <!--Scrypting-->
+  <!--<script src="app/JS/libs/jquery.js"></script>
+  <script src="app/JS/libs/underscore.js"></script>
+  <script src="app/JS/libs/backbone.js"></script>
+  <script src="app/JS/libs/bootstrapJS.js"></script>
+  <script src="app/JS/libs/backboneLocalStorage.js"></script>--!>
+
+</html>
