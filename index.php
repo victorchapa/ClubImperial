f59a671c71d538d9540c2fbb485d248d03f5e272
victor <victor_260690@hotmail.com>
2013-10-09 18:59:16 -0500

    Added normalizer and IEChecker.

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Club Valle Imperial</title>
-    <!--<link rel="stylesheet" href="app/css/bootstrap.css">
-    <link rel="stylesheet" href="app/css/main.css"> --!>
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/main.css">
     <script src="assets/js/libs/jquery.js"></script>
     <script src="assets/js/libs/underscore.js"></script>
     <script src="assets/js/libs/backbone.js"></script>
+    <script src="assets/js/libs/IEChecker.js"></script>
   </head>
   <body>
     <header>
