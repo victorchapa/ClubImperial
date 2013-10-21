faf255565a63ad899d72cd4aae1a053a86e3fc54
isracortes62 <isracortes62@gmail.com>
2013-10-21 17:03:30 -0500

    Added a filter to search images

--- templates/editSocio.js
+++ templates/editSocio.js
@@ -1,18 +1,13 @@
 TEMPLATES = {
-    formSocio:
+    formEditSocio:
         "<form id='formSocio' method='post' action='api/EditSocio.php' enctype='multipart/form-data'>" +
         "<h1>Favor de ingresar los datos correspondientes:</h1>" +
         "<div class='clear'>" +
         "<div class='data1'>" +
-            "<input type='hidden' name='Id' value=''>" +
             "<p>Nombres: <input type='text' name='Nombre' maxlength='30'></p>" +
+            "<p>Apellido Paterno: <input type='text' name='ApellidoP' maxlength='30'></p>" +
+            "<p>Apellido Materno: <input type='text' name='ApellidoM' maxlength='30'></p>" +
             "<p>Fecha de Nacimiento: <input class='datePicker' type='text' name='FNacimiento' placeholder='mm/dd/aaaa' readonly></p>" +
-            "<p>Parentesco: " +
-                "<span><input type='radio' name='Parentesco' value='Padre'> Padre." +
-                 "<input type='radio' name='Parentesco' value='Madre'> Madre." +
-                 "<input type='radio' name='Parentesco' value='Hijo'> Hijo." +
-                 "<input type='radio' name='Parentesco' value='Soltero(a)'> Soltero(a).</span>" +
-            "</p>" +
             "<p>Domicilio: <input type='text' name='Domicilio'></p>" +
             "<p>Manzana: <input type='text' name='Manzana'></p>" +
             "<p>Lote: <input type='text' name='Lote'></p>" +
@@ -33,21 +28,30 @@ TEMPLATES = {
                 "<option value='AB+'>AB+</option>" +
                 "</select>" +
             "</p>" +
-            "<p>Membresia: <select name='Membresia'>" +
+            "<p>Membresia: " +
+            "<span><input type='radio' name='Membresia' value='Propietario'> Propietario." +
+            "<input type='radio' name='Membresia' value='Rentista'> Rentista.</span>" +
+            "</p>" +
+            "<p>Tipo de membresia: <select name='TipoMembresia'>" +
                   "<option value='0'>Elige una opción</option>" +
-                  "<option value='Propietario'>Propietario</option>" +
-                  "<option value='Rentista'>Rentista</option>" +
-                  "<option value='Jr'>Jr</option>" +
                   "<option value='Familiar'>Familiar</option>" +
                   "<option value='Individual'>Individual</option>" +
+                  "<option value='Jr'>Jr</option>" +
                 "</select>" +
             "</p>" +
             "<p>Fecha de Alta: <input class='datePicker' type='text' name='FAlta' placeholder='mm/dd/aaaa' readonly></p>" +
-            "<p>Afiliación: <input type='text' name='Afiliacion' maxlength='30' placeholder='Golfistas, tenistas, etc.'></p>" +
+            "<p>Afiliación: " + 
+                "<span><span><input type='checkbox' name='Afiliacion[]' value='Golf'> Golfista.</span>" +
+                "<span><input type='checkbox' name='Afiliacion[]' value='Futbol'> Futbolista.</span>" +
+                "<span><input type='checkbox' name='Afiliacion[]' value='Tenista'> Tenista.</span><br>" +
+                "<span><input type='checkbox' name='Afiliacion[]' value='Piscina'> Piscina.</span>" +
+                "<span><input type='checkbox' name='Afiliacion[]' value='Gym'> GYM.</span>" +
+                "<span><input type='checkbox' name='Afiliacion[]' value='SPA'> SPA.</span></span>" +
+            "</p>" +
         "</div>" +
         "</div>" +
         "<div class='btnGroup'>" +
-            "<p><input id='upLoad' class='display-none' type='file' name='Foto'><input class='btn btnFunctions btnUpLoad' type='button' value='Subir Foto'>" +
+            "<p><input id='upLoad' class='display-none' type='file' name='Foto' accept='image/jpeg, image/bmp'><input class='btn btnFunctions btnUpLoad' type='button' value='Subir Foto'>" +
             "<input class='btn btnSuccess' type='submit' value='Agregar Usuario'></p>" +
             "<p><span id='fileName'></span></p>" +
         "</div>" +
