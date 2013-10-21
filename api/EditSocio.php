aa47f9911fd9a0525376f6328e119280c28f3341
isracortes62 <isracortes62@gmail.com>
2013-10-21 16:31:52 -0500

    testing upload 2

--- api/EditSocio.php
+++ api/EditSocio.php
@@ -27,6 +27,7 @@
     mysql_select_db($dbName);
 	mysql_query ("SET NAMES 'utf8'");
 	mysql_query("UPDATE socios SET Nombre='$nombre', ApellidoP='$apellidop', ApellidoM='$apellidom', FNacimiento='$fnacimiento', Domicilio= '$domicilio', Manzana = '$manzana', Lote = '$lote', Coto = '$coto', Telefono = '$telefono', Celular = '$celular', Membresia= '$membresia', TipoMembresia= '$tipomembresia', Sangre ='$sangre', FAlta = '$falta', Afiliacion = '$afiliacion' WHERE IdSocio = '$id'");
+	
 	$oldfoto=consultar("SELECT Foto FROM socios WHERE IdSocio='$id'");
 	$oldfoto=$oldfoto[0]["Foto"];
 	if(isset($oldfoto)){
