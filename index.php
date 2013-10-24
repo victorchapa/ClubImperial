1838673420edcd47bff2160fa59b1c69d0e07d38
Victor Chapa C <victor.260690@gmail.com>
2013-10-24 13:54:02 -0500

    Added styling for actionsSocios window and funcionality.

--- index.php
+++ index.php
@@ -56,24 +56,26 @@
           <img src="assets/img/lotLeafs.gif">
         </div>
       </section>
-      <div id="mainDisplayer"></div>
+      <div id="mainDisplayer">
+      </div>
     </div>
-<div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title">Ficha del Socio</h4>
-        </div>
-        <div id="modalDisplayer" class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
+    <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">Ficha del Socio</h4>
+          </div>
+          <div id="modalDisplayer" class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
 
   <!--Scrypting-->
   <script src="assets/js/main.js"></script>
