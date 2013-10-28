03f09b00f909a8cd66fa86929ce6db636bc9183c
isracortes62 <isracortes62@gmail.com>
2013-10-28 12:59:50 -0600

    Database updated, field parentesco added

--- db/socios.sql
+++ db/socios.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-10-2013 a las 18:37:38
+-- Tiempo de generación: 28-10-2013 a las 18:56:58
 -- Versión del servidor: 5.6.12-log
 -- Versión de PHP: 5.4.12
 
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `socios` (
   `Nombre` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
   `ApellidoP` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
   `ApellidoM` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
+  `Parentesco` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `FNacimiento` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `Domicilio` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `Manzana` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
@@ -47,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `socios` (
   `Foto` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
   `Afiliacion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `Cuota` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `Mayor` tinyint(1) NOT NULL,
+  `Mayor` varchar(2) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`IdSocio`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
 
@@ -55,9 +56,9 @@ CREATE TABLE IF NOT EXISTS `socios` (
 -- Volcado de datos para la tabla `socios`
 --
 
-INSERT INTO `socios` (`IdSocio`, `IdPariente`, `Nombre`, `ApellidoP`, `ApellidoM`, `FNacimiento`, `Domicilio`, `Manzana`, `Lote`, `Coto`, `Telefono`, `Celular`, `Correo`, `Membresia`, `TipoMembresia`, `Sangre`, `FAlta`, `Foto`, `Afiliacion`, `Cuota`, `Mayor`) VALUES
-(1, 0, 'Oscar', 'Vargas', 'Muñoz', '24/11/1990', 'domicilio 1', 'manzana 1', 0, 0, 123, 1234567890, '', 'Rentista', 'Individual', 'A', '14/10/2013', 'socios/1/perfil.jpg', 'Golf', '', 0),
-(3, 0, 'Israel', 'Alcantar', 'Cortés', '10/01/2013', 'Domicilio1', '1', 2, 3, 1234567, 2147483647, 'isracortes62@gmail.com', 'Propietario', 'Familiar', 'A+', '10/22/2013', 'socios/3/perfil.jpg', 'Piscina, Gym, SPA', '', 0);
+INSERT INTO `socios` (`IdSocio`, `IdPariente`, `Nombre`, `ApellidoP`, `ApellidoM`, `Parentesco`, `FNacimiento`, `Domicilio`, `Manzana`, `Lote`, `Coto`, `Telefono`, `Celular`, `Correo`, `Membresia`, `TipoMembresia`, `Sangre`, `FAlta`, `Foto`, `Afiliacion`, `Cuota`, `Mayor`) VALUES
+(1, 0, 'Oscar', 'Vargas', 'Muñoz', '', '24/11/1990', 'domicilio 1', 'manzana 1', 0, 0, 123, 1234567890, '', 'Rentista', 'Individual', 'A', '14/10/2013', 'socios/1/perfil.jpg', 'Golf', '', '0'),
+(3, 0, 'Israel', 'Alcantar', 'Cortés', '', '10/01/2013', 'Domicilio1', '1', 2, 3, 1234567, 2147483647, 'isracortes62@gmail.com', 'Propietario', 'Familiar', 'A+', '10/22/2013', 'socios/3/perfil.jpg', 'Piscina, Gym, SPA', '', '0');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
