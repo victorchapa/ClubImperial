3cc9e367717141e09c9874284512c927485c7fb8
isracortes62 <isracortes62@gmail.com>
2013-11-11 17:19:12 -0600

    Working on payment system

---
+++ api/cpago.php
@@ -0,0 +1,42 @@
+<?php
+  setlocale(LC_ALL,"spanish");
+  require("dbConfig.php");
+  include("consultas.php");
+  $nombre = $_POST["Nombre"]." ".$_POST["ApellidoP"]." ".$_POST["ApellidoM"];
+  $names = explode(" ", $nombre);
+  $name = $names[0];
+  $ap = $names[1];
+  $am = $names[2];
+  $dia = strftime("%d");
+  $mes = strftime("%b");
+  $mes= ucfirst($mes);
+  $year = strftime("%Y");
+  $hora = date("g:i a");
+  $servicio = $_POST["Servicio"];
+  $abono = $_POST["Abono"];
+  $total = $_POST["Total"];
+  $deudas = consultar("SELECT * FROM deudas WHERE Nombre = '$nombre' AND Mes = '$mes' AND Year = '$year'");
+  $id = consultar("SELECT IdSocio FROM socios WHERE Nombre= '$name' AND ApellidoP = '$ap' AND ApellidoM = '$am'");
+  $id = $id[0]["IdSocio"];
+  if($deudas){
+    $tabono = ($deudas[0]["Abono"]) + $abono;
+    $ttotal = ($deudas[0]["Total"]) + $total;
+    $conexion =  mysql_connect($serverAddress, $user, $passwd);
+    mysql_select_db($dbName);
+    mysql_query ("SET NAMES 'utf8'");
+    echo "si entra";
+    mysql_query("UPDATE deudas SET Abono = '$tabono', Total = '$ttotal' WHERE Nombre= '$nombre'");
+    mysql_query("INSERT INTO facturas (IdSocio, Servicio, Abono, Total, Hora, Dia, Mes, Year) 
+      VALUES ('$id', '$servicio', '$abono', '$total', '$hora', '$dia', '$mes', '$year')");
+    echo "<script type=text/javascript>window.location.href=\"../deudas.php\";</script>";
+  } else {
+    $conexion =  mysql_connect($serverAddress, $user, $passwd);
+    mysql_select_db($dbName);
+    mysql_query ("SET NAMES 'utf8'");
+    mysql_query("INSERT INTO facturas (IdSocio, Servicio, Abono, Total, Hora, Dia, Mes, Year) 
+      VALUES ('$id', '$servicio', '$abono', '$total', '$hora', '$dia', '$mes', '$year')");
+    mysql_query("INSERT INTO deudas (IdSocio, Nombre, Abono, Total, Mes, Year) VALUES ('$id', '$nombre', '$abono', '$total', '$mes', '$year')");
+    echo "<script type=text/javascript>window.location.href=\"../deudas.php\";</script>";
+  }
+?>
+</html>
---
+++ factura.php
@@ -0,0 +1,56 @@
+<?php 
+  setlocale(LC_ALL,"spanish");
+  include("api/consultas.php");
+  $id=$_GET["id"];
+  $mes = strftime("%b");
+  $mes= ucfirst($mes);
+  $año = strftime("%Y");
+  $total = 0;
+  $pago = 0;
+  $deuda = 0;
+  $facturas = consultar("SELECT * FROM facturas WHERE IdSocio = '$id' AND Mes='$mes' AND Year='$año'");
+  $deudas = consultar("SELECT * FROM deudas WHERE IdSocio = '$id' AND Mes='$mes' AND Year='$año'");
+  $tdeuda = ($deudas[0]["Total"]) - ($deudas[0]["Abono"]);
+  $socio = $deudas[0]["Nombre"];
+  if($facturas){
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+  <h2>Deudas de <?php echo $socio; ?> </h2>
+<table border="1">
+  <thead>
+    <th>Servicio</th>
+    <th>Fecha y hora</th>
+    <th>Abono</th>
+    <th>Deuda</th>
+    <th>Total</th>
+  </thead>
+  <tbody>
+<?php
+    foreach($facturas as $factura){
+      echo "<tr><td>".$factura['Servicio']."</td>";
+      echo "<td>".$factura['Dia']."/".$factura['Mes']."/".$año."</br>".$factura['Hora']."</td>";
+      echo "<td>$".$factura['Abono']."</td>";
+      $deuda = ($factura['Total']) - ($factura['Abono']);
+      echo "<td>$".$deuda."</td>";
+      echo "<td>$".$factura['Total']."</td></tr>";
+    }
+?>
+  <tr><td><b>Total: <b></td>
+    <td></td>
+    <td><b><?php echo "$ ".$deudas[0]["Abono"]; ?><b></td>
+    <td><b><?php echo "$ ".$tdeuda; ?><b></td>
+    <td><b><?php echo "$ ".$deudas[0]["Total"]; ?></b></td></tr>
+  </tbody>
+</table>
+<?php
+  } else{ 
+      echo "No hay movimientos en la cuenta de ".$socio;
+    }
+?>
+  </body>
+</html>
