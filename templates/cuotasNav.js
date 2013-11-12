077a2da9c327c5b55a2ac75de52f24e80cdeba21
isracortes62 <isracortes62@gmail.com>
2013-11-12 15:18:37 -0600

    Created a new template for the navbar

---
+++ templates/cuotasNav.js
@@ -0,0 +1,12 @@
+TEMPLATES.cuotasNav = 
+    "<div class='cobrosContent'>" +
+        "<div class='backgroundNav'>" +
+            "<ul class='navSocio'>" +
+                "<li class='active'><p>CUOTAS</p></li>" +
+                "<li><p>DEUDAS</p></li>" +
+                "<li><p>FACTURA</p></li>" +
+            "</ul>" +
+        "</div>" +
+    	"<div class='cuotaBody'>" +
+    	"</div>" +
+    "</div>";
