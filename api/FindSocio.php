04181dc88db37d74abfbd72593782e1d6fae9d5c
isracortes62 <isracortes62@gmail.com>
2013-11-14 11:57:45 -0600

    Fixed backend of findsocio

--- api/FindSocio.php
+++ api/FindSocio.php
@@ -13,20 +13,24 @@
     if ($type == "1"){
         $socio = consultar("SELECT * FROM socios WHERE IdSocio='$campo'");
     } elseif ($type == "2")  {
-        $socio = consultar("SELECT * FROM socios WHERE Nombre='$nombre'");
+        $name = explode(" ", $nombre);
+        $nombre = $name[0];
+        $ap = $name[1];
+        $am = $name[2];
+        $socio = consultar("SELECT * FROM socios WHERE Nombre='$nombre' AND ApellidoP='$ap' AND ApellidoM='$am'");
     }
   ?>
   <div class="findSocio">
   <?php
-  if($socios){
+  if($socio){
     $idsocio = $socio["0"]["IdSocio"];
     $acceso = consultar("SELECT * FROM historial WHERE IdSocio = '$idsocio' ORDER BY IdHistorial DESC");
-    echo "<img class='fotoSocio' src='".$socios["0"]["Foto"]."'>";
+    echo "<img class='fotoSocio' src='".$socio["0"]["Foto"]."'>";
     echo "<div class='clear'>";
     echo "<div class='data1'>";
-    echo "<p><b>Nombre: </b><label>".$socios["0"]["Nombre"]."</label></p>";
-    echo "<p><b>Apellido P.: </b><label>".$socios["0"]["ApellidoP"]."</label></p>";
-    echo "<p><b>Apellido M.: </b><label>".$socios["0"]["ApellidoM"]."</label></p>";
+    echo "<p><b>Nombre: </b><label>".$socio["0"]["Nombre"]."</label></p>";
+    echo "<p><b>Apellido P.: </b><label>".$socio["0"]["ApellidoP"]."</label></p>";
+    echo "<p><b>Apellido M.: </b><label>".$socio["0"]["ApellidoM"]."</label></p>";
     echo "<p><b>Último acceso: </b>".$acceso["0"]["FechaA"]." a las ".$acceso["0"]["HoraA"]."</p>";
     echo "</div>";
     echo "<a href='access.php?id=".$idsocio."'><input type='button' class='btn-success btn btnAcceso' value='Acceso'></a>";
