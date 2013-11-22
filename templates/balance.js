bc0036573d2ac2fd755f7a2fc272956377790b53
Victor Chapa C <victor.260690@gmail.com>
2013-11-22 16:02:49 -0600

    Added subMenu in the table in order to show the details.

--- templates/balance.js
+++ templates/balance.js
@@ -10,18 +10,22 @@ TEMPLATES.balance =
                 "<th>Socios</th>" +
                 "<th>Cargo</th>" +
                 "<th>Abono</th>" +
-                "<th>Lista</th>" +
             "</tr>" +
           "</thead>" +
           "<tbody>" +
                 "<% _.each(socios, function(socio) { %>" +
-                "<tr id='watchSocio<% print(socio.IdSocio) %>'>" +
+                "<tr class='trSocio' idsocio='<% print(socio.IdSocio); %>' id='watchSocio<% print(socio.IdSocio) %>'>" +
                     "<td><% print(socio.Nombre); %></td>" +
                     "<td>$<% print(socio.Cargo); %></td>" +
                     "<td>$<% print(socio.Abono); %></td>" +
-                    "<td><a class='watchFact' idsocio='<% print(socio.IdSocio); %>' href='#ccuotas/factura?id=<% print(socio.IdSocio); %>'>Ver</a></td>" +
                 "</tr>" +
                 "<% }); %>" +
             "</tbody>" +
             "</table>" +
+            "<div class='emergentWindow actionsSocio'>" +
+                "<div class='idSocio'>" +
+                    "<span class='display-none spanIdSocio'></span>" +
+                    "<p class='showDetails'>Ver Detalles</p>" +
+                "</div>" +
+            "</div>" +  
             "</script>";
