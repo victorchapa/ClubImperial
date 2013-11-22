51d917c8444a344466e4d85ce9af9810edfc96ec
Victor Chapa C <victor.260690@gmail.com>
2013-11-21 18:23:05 -0600

    Added close button to factures window.

--- templates/facturesWindow.js
+++ templates/facturesWindow.js
@@ -1,10 +1,13 @@
 TEMPLATES.facturesWindow =
         "<script type='text/template'>" +
+            "<a href='#ccuotas'>" +
+                "<input class='close' type='button' value='×'/>" +
+            "</a>" +
             "<p>¿Desea generar factura del cobro anterior?</p>" +
             "<a href='api/pdf.php?id=<% print(factureId) %>' target='_blank'>" +
-                "<input class='btn btnFunctions' type='button' value='PDF'/>"+
+                "<input class='btn btnFunctions btnFacture' type='button' value='PDF'/>"+
             "</a>" +
             "<a href='api/excel.php?id=<% print(factureId) %>'>" +
-                "<input class='btn btnFunctions' type='button' value='EXCEL'/>"+
+                "<input class='btn btnFunctions btnFacture' type='button' value='EXCEL'/>"+
             "</a>" +
         "</script>";
