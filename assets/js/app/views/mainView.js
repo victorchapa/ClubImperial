6e02aaf219450f8bf7f50dc9934302092b4296d0
Victor Chapa C <victor.260690@gmail.com>
2013-11-22 12:08:20 -0600

    Changed the view using spefic routes with redirections.

--- assets/js/app/views/mainView.js
+++ assets/js/app/views/mainView.js
@@ -4,33 +4,20 @@ var MainView = Backbone.View.extend({
 
     events: {
         "click .addSocios"                  :   "renderFormSocio",
+        "click .addCuota"                   :   "renderFormCuotas",
+    },
+    
+    
+    initialize: function(){
+        this.router = new ClubImperial.Router();
     },
 
-    initialize: function(){},
-
     renderFormSocio: function(e){
-        var lastActive = $(".active", ".mainNav");
-        $(lastActive).removeClass("active");
-        var addSocio = $("ul", ".mainNav");
-        $("li:nth-child(2)", addSocio).addClass("active");
+        this.router.navigate("#addsocio", {trigger: true, replace: true});
+    },
 
-        if($("#formSocio").length == 0){
-            $("#mainDisplayer").html(_.template(TEMPLATES.formSocio));
-            $(".datePicker").datepicker({
-                changeMonth: true,
-                changeYear: true,
-                changeDay: true
-            });
-            $(".btnUpLoad").on("click", function(){
-                $("#upLoad").click();
-                setInterval(function(){
-                    var name = $('#upLoad').val();
-                    var fileName = name.split("\\");
-                    $('#fileName').text(fileName[2]);
-                },1);
-                return false;
-            });
-        }
+    renderFormCuotas: function(){
+        this.router.navigate("#ccuotas", {trigger: true, replace: true});
     },
 
 });  
