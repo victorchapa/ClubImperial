b47cad39cc2b1127dd27d9aef1c4c8ef479c5f93
isracortes62 <isracortes62@gmail.com>
2013-11-22 18:19:18 -0600

    Added links to export all the payments to excel or pdf

--- api/cargospdf.php
+++ api/cargospdf.php
@@ -2,15 +2,14 @@
 require_once('tcpdf/tcpdf.php');
 include("consultas.php");
 $id = $_GET["id"];
-$recibo = consultar("SELECT * FROM Cargos WHERE IdCargo='$id'");
-//$servicio = $recibo[0]["Servicio"];
-$cargo = $recibo[0]["Cargo"];
-$deuda = $recibo[0]["Deuda"];
-$hora = $recibo[0]["Hora"];
-$d = $recibo[0]["Dia"];
-$m = $recibo[0]["Mes"];
-$y = $recibo[0]["Year"];
-$name = $d."/".$m."/".$y." ".$hora;
+$m = ucfirst($_GET["m"]);
+$y = $_GET["y"];
+$recibo = consultar("SELECT * FROM Cargos WHERE IdSocio='$id' AND Mes='$m' AND Year='$y'");
+$balance = consultar("SELECT * FROM Balance WHERE IdSocio='$id' AND Mes='$m' AND Year='$y'");
+$name = $balance[0]["Nombre"]." ".$balance[0]["Mes"]." ".$balance[0]["Year"];
+$socio = $balance[0]["Nombre"];
+$bcargo = $balance[0]["Cargo"];
+$tcargo = 0;
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
@@ -41,24 +40,41 @@ $pdf->SetFont('helvetica', 'B', 20);
 $pdf->AddPage();
 $pdf->SetFont('helvetica', '', 8);
 $tbl = <<<EOD
-<h2 style= "text-align:center">Recibo de compra</h2>
+<h2 style= "text-align:center">Cargos de $socio $m-$y</h2>
 <table cellspacing="0" cellpadding="1" border="1">
     <tr>
-        <th>Servicio</th>
-        <th>Cargo</th>
-        <th>Deuda</th>
-        <th>Fecha/hora</th>
+        <th><b>Servicio</b></th>
+        <th><b>Fecha/hora</b></th>
+        <th><b>Cargo</b></th>
+        <th><b>Saldo</b></th>
     </tr>
+EOD;
+foreach($recibo as $r){
+    $s = $r["Servicio"];
+    $f = $r["Dia"]."/".$r["Mes"]."/".$r["Year"]." ".$r["Hora"];
+    $c = $r["Cargo"];
+    $d = $r["Deuda"];
+    $tcargo += $c;
+    $tbl .= <<<EOD
+        <tr>
+            <td>$s</td>
+            <td>$f</td>
+            <td>$$c</td>
+            <td>$$d</td>
+        </tr>   
+EOD;
+}
+$tbl .= <<<EOD
     <tr>
-        <td>$servicio</td>
-        <td>$$cargo</td>
-        <td>$$deuda</td>
-        <td>$name</td>
+        <td>Cargo Total</td>
+        <td> </td>
+        <td>$$tcargo</td>
+        <td>$$bcargo</td>
     </tr>
 </table>
 EOD;
 
 $pdf->writeHTML($tbl, true, false, false, false, '');
 //Close and output PDF document
-$pdf->Output('recibo '.$name.'.pdf', 'I');
+$pdf->Output($name.'.pdf', 'I');
 ?>
