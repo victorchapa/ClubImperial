ffdd8054a568f07dbcf96d7f1a2435548fe74807
Victor Chapa C <victor.260690@gmail.com>
2013-11-22 18:28:11 -0600

    Added template and didacated CargosView.

---
+++ assets/js/app/views/socioCargosView.js
@@ -0,0 +1,78 @@
+var SocioCargosView = Backbone.View.extend({
+
+    el: "#mainDisplayer",
+
+    events: {
+        "click .filterBy"   : "filterByMY",
+    },
+
+    initialize: function(){
+        this.socioId = this.options.socioId; 
+
+        var template = TEMPLATES.cuotasNav;
+        var compiledTemplate = _.template($(template).html());
+        var target = {target: "cargos"};
+        $("#mainDisplayer").html(compiledTemplate(target));
+        this.render();
+    },
+
+    render: function(){
+
+        if($("#factures").length != 1){
+            var div = document.createElement("div");
+            div.id = "factures";
+            $("#cuotaBody").append(div);
+        }
+
+        var facturasSocio = new FacturasSocio(this.socioId);
+        var deudaSocio = new DeudaSocio(this.socioId);
+        facturasSocio.fetch({
+            success: function(data){
+                var facturas = data.toJSON();
+                deudaSocio.fetch({
+                    success: function(data){
+                        var data = data.toJSON();
+                        deuda = data[0];
+
+                        var template = TEMPLATES.cargos;
+                        var compiledTemplate = _.template($(template).html());
+                        var socio = {facturas: facturas, deuda: deuda};
+                        $("#factures").html(compiledTemplate(socio));
+                    },
+                });
+
+            },
+        });
+    },
+
+    filterByMY: function(e){
+        var monthSelected = $(".fMonth option:selected").val();
+        var yearSelected = $(".fYear").val();
+        if((monthSelected != 0) && (yearSelected != "")){
+            var newLocation = "#ccuotas/factura?id="+this.socioId+"&mes="+ monthSelected + "&year=" + yearSelected;
+            window.location.href = newLocation;
+        }else if((monthSelected == 0) && (yearSelected != "")){
+            var newLocation = "#ccuotas/factura?id="+this.socioId+"&year="+yearSelected;
+            window.location.href = newLocation;
+        }else if((monthSelected != 0) && (yearSelected == "")){
+            var newLocation = "#ccuotas/factura?id="+this.socioId+"&mes="+monthSelected;
+            window.location.href = newLocation;
+        }else{
+            console.log("Ningun filtro llenado");
+        }
+    },
+
+    kill: function(){
+        // same as this.$el.remove();
+        this.remove();
+
+        // unbind events that are
+        // set on this view
+        this.off();
+
+        // remove all models bindings
+        // made by this view
+        this.model.off( null, null, this );
+    },
+
+});
