1a3d04e407a1a1f3138d3ed72b7e2da6c415382a
isracortes62 <isracortes62@gmail.com>
2013-11-28 16:29:00 -0600

    Modified structure to fit the new styling

--- templates/editSocio.js
+++ templates/editSocio.js
@@ -1,28 +1,24 @@
 TEMPLATES.editSocio =
-        "<script type='text/template'>" + 
+        "<script type='text/template'>" +
         "<form id='editSocioForm' method='post' action='api/EditSocio.php' enctype='multipart/form-data'>" +
         "<h1>Favor de ingresar los datos a Editar:</h1>" +
-        "<div class='clear'>" +
-        "<div class='data1'>" +
             "<input type='hidden' name='id' value='<% print(socio.IdSocio); %>'>" +
             "<div class='fotoSocioEdit'>" +
                 "<img class='photoReload' src='api/<% print(socio.Foto); %>'>" +
             "</div>" +
             "<p><input id='upLoad' class='display-none' type='file' name='Foto'><input class='btn btnFunctions btnUpLoad' type='button' value='Editar Foto'></p>" +
             "<p><span class='display-none' id='fileName'></span></p>" +
-            "<p>Nombres: <input type='text' name='Nombre' maxlength='30' value='<% print(socio.Nombre); %>'></p>" +
-            "<p>Apellido Paterno: <input type='text' name='ApellidoP' maxlength='30' value='<% print(socio.ApellidoP); %>'></p>" +
-            "<p>Apellido Materno: <input type='text' name='ApellidoM' maxlength='30' value='<% print(socio.ApellidoM); %>'></p>" +
+            "<p>Nombres: <input type='text' class='input-sm form-control' name='Nombre' maxlength='30' value='<% print(socio.Nombre); %>'></p>" +
+            "<p>Apellido Paterno: <input type='text' class='input-sm form-control' name='ApellidoP' maxlength='30' value='<% print(socio.ApellidoP); %>'></p>" +
+            "<p>Apellido Materno: <input type='text' class='input-sm form-control' name='ApellidoM' maxlength='30' value='<% print(socio.ApellidoM); %>'></p>" +
             "<p>Fecha de Nacimiento: <input class='datePicker' type='text' name='FNacimiento' placeholder='mm/dd/aaaa' value='<% print(socio.FNacimiento); %>' readonly></p>" +
-            "<p>Domicilio: <input type='text' name='Domicilio' value='<% print(socio.Domicilio); %>'></p>" +
-            "<p>Manzana: <input type='text' name='Manzana' value='<% print(socio.Manzana); %>'></p>" +
-        "</div>" +
-        "<div class='data2'>" +
-            "<p>Lote: <input type='text' name='Lote' value='<% print(socio.Lote); %>'></p>" +
-            "<p>Coto: <input type='text' name='Coto' value='<% print(socio.Coto); %>'></p>" +
-            "<p>Teléfono: <input type='text' name='Telefono' maxlength='10' value='<% print(socio.Telefono); %>'></p>" +
-            "<p>Celular: <input type='text' name='Celular' maxlength='10' value='<% print(socio.Celular); %>'></p>" +
-            "<p>Email: <input type='text' name='Correo' value='<% print(socio.Correo); %>'></p>" +
+            "<p>Domicilio: <input type='text' class='input-sm form-control' name='Domicilio' value='<% print(socio.Domicilio); %>'></p>" +
+            "<p>Manzana: <input type='text' class='input-sm form-control' name='Manzana' value='<% print(socio.Manzana); %>'></p>" +
+            "<p>Lote: <input type='text' class='input-sm form-control' name='Lote' value='<% print(socio.Lote); %>'></p>" +
+            "<p>Coto: <input type='text' class='input-sm form-control' name='Coto' value='<% print(socio.Coto); %>'></p>" +
+            "<p>Teléfono: <input type='text' class='input-sm form-control' name='Telefono' maxlength='10' value='<% print(socio.Telefono); %>'></p>" +
+            "<p>Celular: <input type='text' class='input-sm form-control' name='Celular' maxlength='10' value='<% print(socio.Celular); %>'></p>" +
+            "<p>Email: <input type='text' class='input-sm form-control' name='Correo' value='<% print(socio.Correo); %>'></p>" +
             "<p>Tipo de Sangre: <select name='Sangre'>" +
                 "<option value='<% print(socio.Sangre); %>'><% print(socio.Sangre); %></option>" +
                 "<option value='O-'>O-</option>" +
@@ -94,12 +90,10 @@ TEMPLATES.editSocio =
                     "<input class='btn btnAddPari' type='button' value='+'>" +
                     "<input class='btn btnDelPari' type='button' value='-' disabled>" +
                     "<input class='btn btnEditPari' type='button' value=' 'disabled>" +
-                    "<input type='text' value='' placeholder='¿Algun pariente?' disabled>" +
+                    "<input type='text' class='input-sm form-control' value='' placeholder='¿Algun pariente?' disabled>" +
                 "</div>" +
-        "</div>" +
         "<div class='btnGroup'>" +
             "<p><input class='btn btnSuccess' type='submit' value='Editar Usuario'></p>" +
-        "</div>" +
         "</div>" +
         "</form>" +
         "</script>";
