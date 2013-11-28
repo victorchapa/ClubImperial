1dab20c54af09c9c2c94bbd83f220008dd81f0c4
isracortes62 <isracortes62@gmail.com>
2013-11-28 13:32:54 -0600

    Form to add new service to the catalog

---
+++ nuevoservicio.php
@@ -0,0 +1,9 @@
+<html>
+	<body>
+	</body>
+<form method='post' action='api/nuevoServicio.php'>
+    <p>Servicio: <input type='text' name='servicio'> </p>
+    <p>Cargo: <input type='text' name='cargo'></p>
+    <p><input class='btn btnSuccess' type='submit' value='Agregar Servicio'></p>
+</form>
+</html>
