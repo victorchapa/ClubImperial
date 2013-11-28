7831a0a60270aedf5ca61e8f0932adb1e6fcb40a
isracortes62 <isracortes62@gmail.com>
2013-11-28 13:30:35 -0600

    Added function to charge monthly services

---
+++ api/Servicio.php
@@ -0,0 +1,11 @@
+<?php
+	include("consultas.php");
+	$idsocio = $_POST['idsocio'];
+	$frecuencia = $_POST['frecuencia'];
+	$servicio = $_POST['servicio'];
+	$consultaservicio = consultar("SELECT * FROM servicios WHERE IdServicio = '$servicio'");
+	$ser = $consultaservicio[0]["Servicio"];
+	$cargo = $consultaservicio[0]["Cargo"];
+	$add = add("INSERT INTO cargosf (IdSocio, Frecuencia, Servicio, Cargo)	VALUES ('$idsocio', '$frecuencia', '$ser', '$cargo')");
+	echo "<script type=text/javascript>window.location.href=\"../index.php\";</script>";
+?>
