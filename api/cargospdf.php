37eac457873be0f2000c53278e3f465b55b2b871
isracortes62 <isracortes62@gmail.com>
2013-12-02 11:33:18 -0600

    Fixed the fields of the table

--- api/cargospdf.php
+++ api/cargospdf.php
@@ -8,8 +8,9 @@ $recibo = consultar("SELECT * FROM Cargos WHERE IdSocio='$id' AND Mes='$m' AND Y
 $balance = consultar("SELECT * FROM Balance WHERE IdSocio='$id' AND Mes='$m' AND Year='$y'");
 $name = $balance[0]["Nombre"]." ".$balance[0]["Mes"]." ".$balance[0]["Year"];
 $socio = $balance[0]["Nombre"];
-$bcargo = $balance[0]["Cargo"];
-$tcargo = 0;
+$tc = 0;
+$ta = 0;
+$ts = 0;
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
@@ -46,6 +47,7 @@ $tbl = <<<EOD
         <th><b>Servicio</b></th>
         <th><b>Fecha/hora</b></th>
         <th><b>Cargo</b></th>
+        <th><b>Abono</b></th>
         <th><b>Saldo</b></th>
     </tr>
 EOD;
@@ -53,23 +55,28 @@ foreach($recibo as $r){
     $s = $r["Servicio"];
     $f = $r["Dia"]."/".$r["Mes"]."/".$r["Year"]." ".$r["Hora"];
     $c = $r["Cargo"];
-    $d = $r["Deuda"];
-    $tcargo += $c;
+    $a = $r["Abono"];
+    $sa = $r["Saldo"];
+    $tc += $c;
+    $ta += $a;
+    $ts += $sa;
     $tbl .= <<<EOD
         <tr>
             <td>$s</td>
             <td>$f</td>
             <td>$$c</td>
-            <td>$$d</td>
+            <td>$$a</td>
+            <td>$$sa</td>
         </tr>   
 EOD;
 }
 $tbl .= <<<EOD
     <tr>
         <td>Cargo Total</td>
-        <td> </td>
-        <td>$$tcargo</td>
-        <td>$$bcargo</td>
+        <td></td>
+        <td>$$tc</td>
+        <td>$$ta</td>
+        <td>$$sa</td>
     </tr>
 </table>
 EOD;
