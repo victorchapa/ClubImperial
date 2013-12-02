f58112c236441218414d844aba9649f50b2ed2d2
isracortes62 <isracortes62@gmail.com>
2013-12-02 16:00:29 -0600

    Added new style to the form

--- templates/formSocios.js
+++ templates/formSocios.js
@@ -1,25 +1,27 @@
 TEMPLATES.formSocio =
+"<div class='well margin-none'>" +
+            "<address class='margin-none'>" +
     "<div class='row'>" +
-        "<div class='editSocioForm'>" +
-        "<div class='col-md-12'>" +
-            "<h1>Favor de ingresar los datos correspondientes:</h1>" +
+        "<div class='SocioForm'>" +
+        "<div class='col-md-10'>" +
+            "<h1 class='heading-arrow'>Favor de ingresar los datos correspondientes:</h1>" +
         "</div>" +
         "<div class='col-md-5'>" +
         "<form id='formSocio' method='post' action='api/AddSocio.php' enctype='multipart/form-data'>" +
-            "<p>Nombres: <input type='text' class='input-sm form-control' name='Nombre' maxlength='30'></p>" +
-            "<p>Apellido Paterno: <input type='text' class='input-sm form-control' name='ApellidoP' maxlength='30'></p>" +
-            "<p>Apellido Materno: <input type='text' class='input-sm form-control' name='ApellidoM' maxlength='30'></p>" +
-            "<p>Fecha de Nacimiento: <input class='datePicker input-sm form-control' type='text' name='FNacimiento' placeholder='mm/dd/aaaa' readonly></p>" +
-            "<p>Domicilio: <input type='text' class='input-sm form-control' name='Domicilio'></p>" +
-            "<p>Manzana: <input type='text' class='input-sm form-control' name='Manzana'></p>" +
-            "<p>Lote: <input type='text' class='input-sm form-control' name='Lote'></p>" +
-            "<p>Coto: <input type='text' class='input-sm form-control' name='Coto'></p>" +
+            "<p><span>Nombres: </span><input type='text' class='input-sm form-control' name='Nombre' maxlength='30'></p>" +
+            "<p><span>Apellido Paterno: </span><input type='text' class='input-sm form-control' name='ApellidoP' maxlength='30'></p>" +
+            "<p><span>Apellido Materno: </span><input type='text' class='input-sm form-control' name='ApellidoM' maxlength='30'></p>" +
+            "<p><span>Fecha de Nacimiento: </span><input class='datePicker input-sm form-control' type='text' name='FNacimiento' placeholder='mm/dd/aaaa' readonly></p>" +
+            "<p><span>Domicilio: </span><input type='text' class='input-sm form-control' name='Domicilio'></p>" +
+            "<p><span>Manzana: </span><input type='text' class='input-sm form-control' name='Manzana'></p>" +
+            "<p><span>Lote: </span><input type='text' class='input-sm form-control' name='Lote'></p>" +
+            "<p><span>Coto: </span><input type='text' class='input-sm form-control' name='Coto'></p>" +
         "</div>" +
         "<div class='col-md-5'>" +
-            "<p>Teléfono: <input type='text' class='input-sm form-control' name='Telefono' maxlength='10'></p>" +
-            "<p>Celular: <input type='text' class='input-sm form-control' name='Celular' maxlength='10'></p>" +
-            "<p>Email: <input type='text' class='input-sm form-control' name='Correo'></p>" +
-            "<p>Tipo de Sangre: <select name='Sangre' class='input-sm form-control'>" +
+            "<p><span>Teléfono: </span><input type='text' class='input-sm form-control' name='Telefono' maxlength='10'></p>" +
+            "<p><span>Celular: </span><input type='text' class='input-sm form-control' name='Celular' maxlength='10'></p>" +
+            "<p><span>Email: </span><input type='text' class='input-sm form-control' name='Correo'></p>" +
+            "<p><span>Tipo de Sangre: </span><select name='Sangre' class='input-sm form-control'>" +
                 "<option value='0'>Elige el tipo</option>" +
                 "<option value='O-'>O-</option>" +
                 "<option value='O+'>O+</option>" +
@@ -31,29 +33,30 @@ TEMPLATES.formSocio =
                 "<option value='AB+'>AB+</option>" +
                 "</select>" +
             "</p>" +
-            "<p>Membresia: </p>" +
-            "<p><input type='radio' name='Membresia' value='Propietario'> Propietario. " +
+            "<p><span>Membresia: </span>" +
+            "<input type='radio' name='Membresia' value='Propietario'> Propietario. " +
             "<input type='radio' name='Membresia' value='Rentista'> Rentista.</p>" +
-            "</p>" +
-            "<p>Tipo de membresia: <select name='TipoMembresia' class='input-sm form-control'>" +
+            "<p><span>Tipo de membresia: </span><select name='TipoMembresia' class='input-sm form-control'>" +
                   "<option value='0'>Elige una opción</option>" +
                   "<option value='Familiar'>Familiar</option>" +
                   "<option value='Individual'>Individual</option>" +
                   "<option value='Jr'>Jr</option>" +
                 "</select>" +
             "</p>" +
-            "<p>Fecha de Alta: <input class='datePicker input-sm form-control' type='text' name='FAlta' placeholder='mm/dd/aaaa' readonly></p>" +
-            "<p>Afiliación: </p>" +
-                "<p><input type='checkbox' name='Afiliacion[]' value='Golf'> Golfista. " +
+            "<p><span>Fecha de Alta: </span><input class='datePicker input-sm form-control' type='text' name='FAlta' placeholder='mm/dd/aaaa' readonly></p>" +
+            "<span>Afiliación: </span>" +
+                "<input type='checkbox' name='Afiliacion[]' value='Golf'> Golfista. " +
                 "<input type='checkbox' name='Afiliacion[]' value='Futbol'> Futbolista. " +
-                "<input type='checkbox' name='Afiliacion[]' value='Tenista'> Tenista. </p>" +
-                "<p><input type='checkbox' name='Afiliacion[]' value='Piscina'> Piscina. " +
+                "<input type='checkbox' name='Afiliacion[]' value='Tenista'> Tenista. " +
+                "<span></span><input type='checkbox' name='Afiliacion[]' value='Piscina'> Piscina. " +
                 "<input type='checkbox' name='Afiliacion[]' value='Gym'> GYM. " +
-                "<input type='checkbox' name='Afiliacion[]' value='SPA'> SPA. </span></p>" +
+                "<input type='checkbox' name='Afiliacion[]' value='SPA'> SPA. " +
         "</div>" +
-        "<div class='col-md-8'>" +
-            "<p><input id='upLoad' class='display-none' type='file' name='Foto' accept='image/jpeg, image/bmp'><input class='btn btnFunctions btnUpLoad' type='button' value='Subir Foto'>" +
-            "<input class='btn btn-success' type='submit' value='Agregar Usuario'></p>" +
+        "<div class='col-md-10'>" +
+            " <input class='btn btn-success' type='submit' value='Agregar Usuario'>" +
+            "<input id='upLoad' class='display-none' type='file' name='Foto' accept='image/jpeg, image/bmp'><input class='btn btn-primary' type='button' value='Subir Foto'>" +
             "<p><span id='fileName'></span></p>" +
+            "</form>" +
         "</div>" +
-        "</div>";
+    "</address>" +
+"</div>";
