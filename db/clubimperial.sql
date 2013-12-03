9db91885e97987c3ca39c5a8509fad522c4d01f8
isracortes62 <isracortes62@gmail.com>
2013-12-03 15:29:18 -0600

    Database updated

--- db/clubimperial.sql
+++ db/clubimperial.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 29-11-2013 a las 23:05:50
+-- Tiempo de generación: 03-12-2013 a las 21:27:12
 -- Versión del servidor: 5.6.12-log
 -- Versión de PHP: 5.4.12
 
@@ -37,16 +37,16 @@ CREATE TABLE IF NOT EXISTS `balance` (
   `Mes` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `Year` int(11) NOT NULL,
   PRIMARY KEY (`IdBalance`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=29 ;
 
 --
 -- Volcado de datos para la tabla `balance`
 --
 
 INSERT INTO `balance` (`IdBalance`, `IdSocio`, `Nombre`, `Abono`, `Cargo`, `Mes`, `Year`) VALUES
-(8, 1, 'Oscar Vargas Muñoz', 0, 3000, 'Nov', 2013),
-(9, 3, 'Israel Alcantar Cortés', 500, 500, 'Nov', 2013),
-(10, 4, 'Victor Chapa Clavillo', 3400, 2800, 'Nov', 2013);
+(26, 1, 'Oscar Vargas Muñoz', 0, 4500, 'Dic', 2013),
+(27, 3, 'Israel Alcantar Cortés', 2100, 2700, 'Dic', 2013),
+(28, 4, 'Victor Chapa Clavillo', 0, 0, 'Dic', 2013);
 
 -- --------------------------------------------------------
 
@@ -66,18 +66,18 @@ CREATE TABLE IF NOT EXISTS `cargos` (
   `Mes` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `Year` int(11) NOT NULL,
   PRIMARY KEY (`IdCargo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=45 ;
 
 --
 -- Volcado de datos para la tabla `cargos`
 --
 
 INSERT INTO `cargos` (`IdCargo`, `IdSocio`, `Servicio`, `Cargo`, `Abono`, `Saldo`, `Hora`, `Dia`, `Mes`, `Year`) VALUES
-(17, 1, 'Carro Bar', 0, 0, 0, '12:24 am', '28', 'Nov', 2013),
-(20, 3, 'Mensualidad', 0, 0, 0, '12:49 am', '28', 'Nov', 2013),
-(21, 4, 'Hoyo 6', 2000, 2000, 0, '4:11 pm', '28', 'Nov', 2013),
-(22, 4, 'Velaria', 1000, 1000, 0, '4:19 pm', '28', 'Nov', 2013),
-(23, 3, 'Caros', 500, 500, 0, '12:26 am', '29', 'Nov', 2013);
+(38, 1, 'Mensualidad', 1500, 0, 1500, '', '03', 'Dic', 2013),
+(41, 3, 'Carro Bar', 1000, 700, 300, '7:08 pm', '03', 'Dic', 2013),
+(42, 3, 'Velaria', 1000, 500, 500, '7:09 pm', '03', 'Dic', 2013),
+(43, 3, 'Carro Bar', 200, 0, 200, '7:48 pm', '03', 'Dic', 2013),
+(44, 3, 'Carro Bar', 500, 0, 500, '7:50 pm', '03', 'Dic', 2013);
 
 -- --------------------------------------------------------
 
@@ -122,6 +122,51 @@ CREATE TABLE IF NOT EXISTS `historial` (
 
 INSERT INTO `historial` (`IdHistorial`, `IdSocio`, `Nombre`, `FechaA`, `HoraA`) VALUES
 (1, 3, 'Israel Alcantar Cortés', '13/11/2013', '11:31 am');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recurrentes`
+--
+
+CREATE TABLE IF NOT EXISTS `recurrentes` (
+  `IdServicio` int(11) NOT NULL AUTO_INCREMENT,
+  `Servicio` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Cargo` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`IdServicio`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `recurrentes`
+--
+
+INSERT INTO `recurrentes` (`IdServicio`, `Servicio`, `Cargo`) VALUES
+(1, 'Mensualidad', 1500),
+(2, 'Mantenimiento', 1200);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `saldos`
+--
+
+CREATE TABLE IF NOT EXISTS `saldos` (
+  `IdSaldo` int(11) NOT NULL AUTO_INCREMENT,
+  `IdSocio` int(11) NOT NULL,
+  `Nombre` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `SaldoAnt` int(11) NOT NULL DEFAULT '0',
+  `SaldoAct` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`IdSaldo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `saldos`
+--
+
+INSERT INTO `saldos` (`IdSaldo`, `IdSocio`, `Nombre`, `SaldoAnt`, `SaldoAct`) VALUES
+(1, 1, 'Oscar Vargas Muñoz', 0, 4500),
+(2, 3, 'Israel Alcantar Cortés', 1000, 1500),
+(3, 4, 'Victor Chapa Clavillo', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -196,7 +241,7 @@ CREATE TABLE IF NOT EXISTS `socios` (
 --
 
 INSERT INTO `socios` (`IdSocio`, `IdPariente`, `Nombre`, `ApellidoP`, `ApellidoM`, `Filtro`, `Parentesco`, `FNacimiento`, `Domicilio`, `Manzana`, `Lote`, `Coto`, `Telefono`, `Celular`, `Correo`, `Membresia`, `TipoMembresia`, `Sangre`, `FAlta`, `Foto`, `Afiliacion`, `Cuota`, `Mayor`) VALUES
-(1, 0, 'Oscar', 'Vargas', 'Muñoz', 'Oscar Vargas Muñoz', '', '24/11/1990', 'domicilio 1', 'manzana 1', 1, 1, 123, 1234567890, 'darckoso@hotmail.com', 'Propietario', 'Individual', 'AB-', '14/10/2013', 'socios/1/perfil.jpg', 'Golf', '', '0'),
+(1, 0, 'Oscar', 'Vargas', 'Muñoz', 'Oscar Vargas Muñoz', '', '24/11/1990', 'domicilio 1', 'manzana 1', 1, 1, 123, 1234567890, 'darckoso@hotmail.com', 'Propietario', 'Individual', 'AB-', '14/10/2013', 'socios/1/1679014-hachiko200505_2.jpg', 'Golf', '', '0'),
 (3, 0, 'Israel', 'Alcantar', 'Cortés', 'Israel Alcantar Cortés', '', '10/01/2013', 'Domicilio1', '1', 2, 3, 1234567, 2147483647, 'isracortes62@gmail.com', 'Propietario', 'Familiar', 'A+', '10/22/2013', 'socios/3/perfil.jpg', 'Piscina, Gym, SPA', '', '0'),
 (4, 1, 'Victor', 'Chapa', 'Clavillo', 'Victor Chapa Clavillo', 'Esposo', '26/06/1990', 'Casa 1', '1', 1, 1, 1234567, 1234567890, 'victor_260690@hotmail.com', 'Propietario', 'Jr', 'O+', '28/10/2013', 'socios/4/perfil.jpg', 'Gym', '', 'Si');
 
