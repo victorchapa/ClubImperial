3e1edcf58ed5219df00b5a8500d770c2d851d78d
isracortes62 <isracortes62@gmail.com>
2013-12-03 15:25:39 -0600

    Added function to fill saldos

--- api/pagarf.php
+++ api/pagarf.php
@@ -10,7 +10,18 @@
     $saldo = $cargos[0]["Saldo"] - $abono;
 	$balance = consultar("SELECT Cargo FROM balance WHERE IdSocio= '$ids' AND Mes='$mes' AND Year='$year'");
 	$abonototal = $balance[0]["Cargo"] + $abono;
+	$anterior = 0;
+	$saldototal = consultar("SELECT * FROM cargos WHERE IdSocio = '$ids' AND Mes = '$mes' AND Year = '$year'");
+    foreach($saldototal as $stotal){
+      $anterior += $stotal["Saldo"];
+    }
     $add = add("UPDATE cargos SET Saldo = '$saldo', Abono = '$tabono' WHERE IdCargo = '$idf'");
     $add = add("UPDATE balance SET Abono = '$abonototal' WHERE IdSocio = '$ids' AND Mes='$mes' AND Year='$year'");
+    $actual = 0;
+    $saldototal = consultar("SELECT * FROM cargos WHERE IdSocio = '$ids' AND Mes = '$mes' AND Year = '$year'");
+    foreach($saldototal as $stotal){
+      $actual += $stotal["Saldo"];
+    }
+    $update = add("UPDATE saldos SET SaldoAnt = '$anterior', SaldoAct ='$actual' WHERE IdSocio = '$ids'");
     echo "<script type=text/javascript>window.location.href=\"../index.php#ccuotas/factura?id=$ids\";</script>";
 ?>
