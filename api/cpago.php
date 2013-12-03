d2c86c88109c1c771e80cf9f0fd52081eee251ca
isracortes62 <isracortes62@gmail.com>
2013-12-03 15:25:39 -0600

    Added function to fill saldos

--- api/cpago.php
+++ api/cpago.php
@@ -21,26 +21,27 @@
   $deudas = consultar("SELECT * FROM balance WHERE Nombre = '$nombre' AND Mes = '$mes' AND Year = '$year'");
   $id = consultar("SELECT IdSocio FROM socios WHERE Nombre= '$name' AND ApellidoP = '$ap' AND ApellidoM = '$am'");
   $id = $id[0]["IdSocio"];
-  if($deudas){
     $diferencia = $total - $abono;
     $balance = ($deudas[0]["Cargo"]) + $total;
     $tabono = ($deudas[0]["Abono"]) + $abono;
     $add = add("UPDATE balance SET Abono = '$tabono', Cargo = '$balance' WHERE Nombre= '$nombre' AND Mes = '$mes' AND Year = '$year'");
+    $anterior = 0;
+    $saldototal = consultar("SELECT * FROM cargos WHERE IdSocio = '$id' AND Mes = '$mes' AND Year = '$year'");
+    foreach($saldototal as $stotal){
+      $anterior += $stotal["Saldo"];
+    }
+    $update = add("UPDATE saldos SET SaldoAnt = '$anterior', SaldoAct ='$actual' WHERE IdSocio = '$id'");
     $conexion =  mysql_connect($serverAddress, $user, $passwd);
     mysql_select_db($dbName);
     mysql_query ("SET NAMES 'utf8'");
     mysql_query("INSERT INTO cargos (IdSocio, Servicio, Cargo, Abono, Saldo, Hora, Dia, Mes, Year) 
       VALUES ('$id', '$servicio', '$total', $abono, $diferencia, '$hora', '$dia', '$mes', '$year')");
     $idf= mysql_insert_id();
+    $actual = 0;
+    $saldototal = consultar("SELECT * FROM cargos WHERE IdSocio = '$id' AND Mes = '$mes' AND Year = '$year'");
+    foreach($saldototal as $stotal){
+      $actual += $stotal["Saldo"];
+    }
+    $update = add("UPDATE saldos SET SaldoAnt = '$anterior', SaldoAct ='$actual' WHERE IdSocio = '$id'");
     echo "<script type=text/javascript>window.location.href=\"../index.php#ccuotas?fid=$idf\";</script>";
-  } else {
-    $conexion =  mysql_connect($serverAddress, $user, $passwd);
-    mysql_select_db($dbName);
-    mysql_query ("SET NAMES 'utf8'");
-    mysql_query("INSERT INTO cargos (IdSocio, Servicio, Cargo, Abono, Saldo, Hora, Dia, Mes, Year) 
-      VALUES ('$id', '$servicio', '$total', $abono, $diferencia, '$hora', '$dia', '$mes', '$year')");
-    $idf= mysql_insert_id();
-    $add = add("INSERT INTO balance (IdSocio, Nombre, Abono, Cargo, Mes, Year) VALUES ('$id', '$nombre', '$tabono', '$balance', '$mes', '$year')");
-    echo "<script type=text/javascript>window.location.href=\"../index.php#ccuotas?fid=$idf\";</script>";
-  }
 ?>
